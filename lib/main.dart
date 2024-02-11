fa956f9d3d2e42f08075a3c6ffb7afff2fc4426d
rlazarteg <rlazarteg@hotmail.com>
2024-02-11 11:38:08 -0500

    se agrego un color de borde

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
 
-        title: Text(widget.title),
+        title: Text(widget.title, style: TextStyle(backgroundColor: Colors.blueGrey),),
       ),
       body: Center(
 
