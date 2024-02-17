67cb09a52c53a0b4061be83b72bb9727a45c97bd
rlazarteg <rlazarteg@hotmail.com>
2024-02-17 12:29:15 -0500

    Se cambio el contador empezando en 10 y aumento de 10 en 10

--- lib/main.dart
+++ lib/main.dart
@@ -35,12 +35,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counter = 10;
 
   void _incrementCounter() {
     setState(() {
 
-      _counter++;
+      _counter+=10;
     });
   }
 
