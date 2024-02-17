9f0559f21f6065134b49db6a50e9885cbc72dda7
rlazarteg <rlazarteg@hotmail.com>
2024-02-17 15:05:52 -0500

    Contador desde 1

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 10;
+  int _counter = 1;
 
   void _incrementCounter() {
     setState(() {
